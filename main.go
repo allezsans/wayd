99524c6994a89bb201107ae555636ac4ab67b4ad
allezsans <allez.sans.question@gmail.com>
2017-08-06 18:42:47 +0900

    Implement command args

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"os"
 	"log"
 
@@ -12,10 +13,17 @@ func main() {
 	api := slack.New(token)
 	logger := log.New(os.Stdout, "slack-bot: ", log.Lshortfile|log.LstdFlags)
 	slack.SetLogger(logger)
-	const (
-		statusText = "test"
-		statusEmoji = ":100:"
+	
+	var (
+		statusText string
+		statusEmoji string
 	)
+
+	flag.StringVar(&statusText, "text", "default", "customText")
+	flag.StringVar(&statusEmoji, "emoji", "+1", "customEmoji")
+	flag.Parse()
+	statusEmoji = ":" + statusEmoji + ":"
+
 	if err := api.SetUserCustomStatus(statusText, statusEmoji); err != nil {
 		logger.Fatalf(`SetUserCustomStatus(%q, %q) = %#v`, statusText, statusEmoji, err)
 	}
